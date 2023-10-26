cfe03e89dae64f668333df5bf4a07d367fab4fdb
Enrique <e.holguin993@gmail.com>
2023-10-26 13:53:00 -0500

    add feature search on [movieId]/page

--- src/app/movie/[movieId]/page.tsx
+++ src/app/movie/[movieId]/page.tsx
@@ -1,8 +1,5 @@
-import { Characters } from '@/app/types/characters';
+import Link from 'next/link';
 import { getCharsByMovie } from '../../../../services/getCharsByMovie';
-
-
-
 
 async function page({ params }: {
     params: {
@@ -12,14 +9,19 @@ async function page({ params }: {
     console.log(params)
     const { movieId } = params
     const characters = await getCharsByMovie(movieId)
-    console.log(characters)
     return (
         <div>
             <h1>{ }</h1>
-            <h2>{characters?.data
-                ? characters.data.map((data) => data.character.name)
-                : 'ERROR, página no encontrada'}
-            </h2>
+            <h2 className='font-bold'>Characters</h2>
+            {characters && characters.data.length > 0
+                ? <ul className='grid grid-cols-5 place-items-center'>
+                    {characters.data.map((data, index) => {
+                        return <li key={data.character.mal_id}>
+                            <Link href={`/characters/${data.character.mal_id}/`}><h3>{data.character.name}</h3></Link>
+                        </li>
+                    })}
+                </ul>
+                : 'No hay personas para mostrar'}
         </div>
     )
 }
